7a1df08dcd14d58a56d05ca9b5666b99f67f2c20
Brittain Hard <brittainhard@gmail.com>
2015-09-11 13:38:02 -0500

    Reformatting init args.

--- source/scraper/spiders/scrapers.py
+++ source/scraper/spiders/scrapers.py
@@ -2,12 +2,9 @@ import scrapy
 
 
 class BaseScraper(scrapy.Spider):
-    name = 'base'
+    name = "base"
 
-    def __init__(self, allowed_domains='', start_urls=[], *args, **kwargs):
+    def __init__(self, allowed_domains=[], start_urls=[], *args, **kwargs):
         self.allowed_domains = allowed_domains
         self.start_urls = start_urls
         super(BaseScraper, self).__init__(*args, **kwargs)
-
-    def parse(self, response):
-        print(response)
