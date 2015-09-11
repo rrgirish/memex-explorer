da518d334d69d6c9c641b8f205ddb622fb53b0da
Brittain Hard <brittainhard@gmail.com>
2015-09-11 10:53:05 -0500

    Working constructor.

--- source/scraper/spiders/scrapers.py
+++ source/scraper/spiders/scrapers.py
@@ -4,5 +4,10 @@ import scrapy
 class BaseScraper(scrapy.Spider):
     name = 'base'
 
-    def __init__(self, start_urls=[], *args, **kwargs):
+    def __init__(self, allowed_domains='', start_urls=[], *args, **kwargs):
+        self.allowed_domains = allowed_domains
         self.start_urls = start_urls
+        super(BaseScraper, self).__init__(*args, **kwargs)
+
+    def parse(self, response):
+        print(response)
